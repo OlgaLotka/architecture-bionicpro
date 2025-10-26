7b03c853269732bdd448ccb969ddf89f6cf268db
Olga Lotka <Lotka_a@mail.ru>
2025-10-26 17:35:21 +0300

    fix dag

--- dag/sql/init-bionicpro-db.sql
+++ dag/sql/init-bionicpro-db.sql
@@ -6,7 +6,7 @@ CREATE TABLE public.device (
 	id int NOT NULL,
 	device_name varchar NULL,
 	device_number varchar NULL,
-	client_id varchar null,
+	client_id int null,
 	CONSTRAINT device_pk PRIMARY KEY (id)
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE public.sensor (
 	id int NOT NULL,
 	sensor_name varchar NULL,
 	sensor_number varchar NULL,
-	device_id varchar NULL,
+	device_id int NULL,
 	CONSTRAINT sensor_pk PRIMARY KEY (id)
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE public.sensor_data (
 	"date" timestamp NULL,
 	action_type varchar NULL,
 	duration varchar NULL,
-	sensor_id varchar NULL,
+	sensor_id int NULL,
 	CONSTRAINT sensor_data_pk PRIMARY KEY (id)
 );
 
---
+++ dag/sql/init-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.data (
+	name varchar NULL,
+	"number" varchar NULL,
+	client_id varchar NULL,
+	"name" varchar NULL,
+	"number" varchar NULL,
+	device_id varchar NULL,
+	"date" timestamp NULL,
+	"action" varchar NULL,
+	duration varchar NULL,
+	sensor_id varchar NULL,
+);
