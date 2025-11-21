0585b4c8bcc633aaa8a5f431bbd18cc181d9c975
Olga Lotka <Lotka_a@mail.ru>
2025-11-21 21:10:43 +0300

    fix redis + add screen

--- bionicpro-auth/src/main/java/org/example/config/RedisConfig.java
+++ bionicpro-auth/src/main/java/org/example/config/RedisConfig.java
@@ -1,8 +1,12 @@
 package org.example.config;
 
 import org.example.domain.LoginResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,10 +15,19 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
     }
+
     @Bean
     public RedisTemplate<String, LoginResponseMessage> redisTemplate() {
         final RedisTemplate<String, LoginResponseMessage> template = new RedisTemplate<String, LoginResponseMessage>();
